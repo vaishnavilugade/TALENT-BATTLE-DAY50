367795c81c1b44436a721dbf54451ade53a4eaf1
Vaishnavi Lugade <108423518+vaishnavilugade@users.noreply.github.com>
2023-01-07 11:36:37 +0530

    Add files via upload

---
+++ Day50.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Day50
+{
+static long SumOfSquare(int arr[],int n)
+ {
+ long sum = 0;
+ for(int i = 0 ; i<n ; i++)
+ {
+ sum = sum + arr[i]*arr[i];
+ }
+ return sum;
+ }
+public static void main(String[] args) throws java.lang.Exception
+{
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int arr[] = new int[n];
+for(int i = 0 ; i<n ; i++)
+{
+arr[i] = sc.nextInt();
+}
+System.out.print(SumOfSquare(arr,n));
+}
+}
+
